4364967ee4b53dc7e3537f8fa91feabff960f847
HyunJungC-Dev <52691074+HyunJungC-Dev@users.noreply.github.com>
2021-03-22 20:10:44 +0900

    add remark

--- programmers/skill_test_level1/solution1.py
+++ programmers/skill_test_level1/solution1.py
@@ -1,6 +1,6 @@
 def solution(s):
-    cnt_p = 0
-    cnt_y = 0
+    cnt_p = 0 # p와 P의 개수를 저장 
+    cnt_y = 0 # y와 Y의 개수를 저장
     for i in s:
         if i == 'p' or i == 'P':
             cnt_p += 1
