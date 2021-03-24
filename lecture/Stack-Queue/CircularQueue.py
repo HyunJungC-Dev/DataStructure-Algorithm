b1a5637c577e3715fb96d0d108f4cacdcce27882
HyunJung <HyunJungC.Dev@gmail.com>
2021-03-24 16:38:21 +0900

    fix
    fix

--- lecture/Stack-Queue/CircularQueue.py
+++ lecture/Stack-Queue/CircularQueue.py
@@ -9,57 +9,19 @@ class CircularQueue:
         self.array = array.array('l', [0]*capacity)
 
     def put(self, value):
-        if self.rear == self.capacity:
-            print("OverFlow")
-            return False
-        else:
-            self.array[self.rear] = value
-            self.rear = (self.rear + 1) % self.capacity
-            return True
+        self.array[self.rear] = value
+        self. rear = (self.rear + 1) % self.capacity
 
     def get(self):
-        if self.front > self.rear:
-            return False
-        else:
-            get_value = self.array[self.front]
-            self.front = (self.front+1) % self.capacity
-            return get_value
+        value = self.array[self.front]
+        self.front = (self.front + 1) % self.capacity
+        return value
 
     def peek(self):
         return self.array[self.front]
 
     def print(self):
-        print("<-", end='')
-        curr = self.front
-        while curr < self.rear:
-            print(self.array[curr], end=' ')
-            curr += 1
-        print("<-", end='')
-        print(" ( Capacity : ", self.capacity, ")")
+        pass
 
 
 myCircularQueue = CircularQueue(10)
-
-print("1. Queue에 10 삽입")
-myCircularQueue.put(10)
-print(myCircularQueue.array)
-myCircularQueue.print()
-print("---------------------------------------")
-
-print("2. Queue에 20 삽입")
-myCircularQueue.put(20)
-print(myCircularQueue.array)
-myCircularQueue.print()
-print("---------------------------------------")
-
-print("3. Queue에서 peek")
-print(myCircularQueue.peek())
-print(myCircularQueue.array)
-myCircularQueue.print()
-print("---------------------------------------")
-
-print("4. Queue에서 get")
-print(myCircularQueue.get())
-print(myCircularQueue.array)
-myCircularQueue.print()
-print("---------------------------------------")
