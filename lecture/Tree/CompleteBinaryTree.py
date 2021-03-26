839035f3e272c9f739d76acf6da7498f5e1e230b
HyunJung <HyunJungC.Dev@gmail.com>
2021-03-26 15:43:24 +0900

    feat: add preorder
     An iterative process to print preorder traversal of Binary tree is node -> left -> right

--- lecture/Tree/CompleteBinaryTree.py
+++ lecture/Tree/CompleteBinaryTree.py
@@ -3,10 +3,20 @@ import array
 
 class BinaryTree:
     def __init__(self, arr):
-        self.array = array.array('l', arr)
+        # 배열의 0번 index는
+        # 계산의 편의성을 위해 사용하지 않는다.
+        self.array = array.array('l', [-1]+arr)
 
-    def preorder(self):
-        pass
+    def preorder(self, idx):
+        if idx > len(self.array)-1:
+            return True
+        print(self.array[idx])
+        left_idx = idx*2
+        right_idx = idx*2+1
+        if left_idx <= len(self.array) - 1:
+            self.preorder(left_idx)
+        if right_idx <= len(self.array)-1:
+            self.preorder(right_idx)
 
     def inorder(self):
         pass
@@ -19,3 +29,10 @@ class BinaryTree:
 
     def dfs(self, value):
         return False
+
+
+arr = [n for n in range(1,11)]
+mybt = BinaryTree(arr)
+print(mybt.array.tolist())
+print("# Preorder")
+mybt.preorder(1)
