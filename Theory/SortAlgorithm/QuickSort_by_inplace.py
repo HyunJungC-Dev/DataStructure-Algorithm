2a3f44a113ff24aec92cbc196b7d236a9e345be5
HyunJung <HyunJungC.Dev@gmail.com>
2021-03-31 20:47:29 +0900

    fix: rename variable
    rename variable

--- Theory/SortAlgorithm/QuickSort_by_inplace.py
+++ Theory/SortAlgorithm/QuickSort_by_inplace.py
@@ -4,29 +4,29 @@ def quick_sort(x):
             return
 
         pivot_idx = (left_first_idx+right_last_idx)//2
-        curr_first_idx = left_first_idx
-        curr_last_idx = right_last_idx
+        bias_left_idx = left_first_idx
+        bias_right_idx = right_last_idx
 
         # 왼쪽 정렬
-        while curr_first_idx < pivot_idx:
-            if x[curr_first_idx] > x[pivot_idx]:
-                tmp = x[curr_first_idx]
-                for i in range(curr_first_idx+1, right_last_idx+1):
+        while bias_left_idx < pivot_idx:
+            if x[bias_left_idx] > x[pivot_idx]:
+                tmp = x[bias_left_idx]
+                for i in range(bias_left_idx+1, right_last_idx+1):
                     x[i-1] = x[i]  # 앞으로 땡김
                 pivot_idx -= 1
                 x[right_last_idx] = tmp
             else:
-                curr_first_idx += 1
+                bias_left_idx += 1
         # 오른쪽 정렬
-        while pivot_idx < curr_last_idx:
-            if x[curr_last_idx] < x[pivot_idx]:
-                tmp = x[curr_last_idx]
-                for i in range(curr_last_idx, left_first_idx, -1):
+        while pivot_idx < bias_right_idx:
+            if x[bias_right_idx] < x[pivot_idx]:
+                tmp = x[bias_right_idx]
+                for i in range(bias_right_idx, left_first_idx, -1):
                     x[i] = x[i-1]  # 뒤로 밈
                 pivot_idx += 1
                 x[left_first_idx] = tmp
             else:
-                curr_last_idx -= 1
+                bias_right_idx -= 1
         recursion(x, left_first_idx, pivot_idx-1)
         recursion(x, pivot_idx+1, right_last_idx)
     recursion(x, 0, len(x)-1)
