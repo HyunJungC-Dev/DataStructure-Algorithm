4614340147b9ee9b9b4f1e84cb97a9e37161c8c3
HyunJung <HyunJungC.Dev@gmail.com>
2021-04-01 21:01:36 +0900

    fix: Paass efficiency test
    can pass efficiency test

--- Programmers/level1/완주하지못한선수.py
+++ Programmers/level1/완주하지못한선수.py
@@ -1,7 +1,7 @@
 def solution(participant, completion):
-    for person in participant:
-        if person in completion:
-            completion.remove(person)
-        else:
-            answer = person
-    return answer
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[-1]
