5f9ded370cef45147905a251ec865b399d1c7ccd
HyunJung <HyunJungC.Dev@gmail.com>
2021-04-02 11:38:36 +0900

    docs: Add remark
    Add remark in merge_sort() function

--- Theory/SortAlgorithm/MergeSort.py
+++ Theory/SortAlgorithm/MergeSort.py
@@ -1,23 +1,27 @@
-
 def merge_sort(x, first=0, last=-1, arr=None):
     if last == -1:
         last = len(x)-1
     if arr is None:
         arr = [0]*len(x)
+    # 종료 조건
     if first >= last:  # 같으면 길이가 1인 것 = 길이가 1일때까지 반으로 쪼갠다.
         return
 
+    # recursion 부분
     mid = (first+last)//2
     merge_sort(x, first, mid, arr)
     merge_sort(x, mid+1, last, arr)
-    i, j = first, mid+1
+
+    # 왼쪽과 오른쪽 Merge 부분
+    i, j = first, mid+1  # 왼쪽 시작idx와 오른쪽 시작 idx
     for k in range(first, last+1):
-        if j > last:
-            arr[k] = x[i]
+        if j > last:  # 오른쪽에 더이상 붙일 것이 없다.
+            arr[k] = x[i]  # 남은 왼쪽을 붙임
             i += 1
-        elif i > mid:
-            arr[k] = x[j]
+        elif i > mid:  # 왼쪽 것이 더이상 붙일 것이 없다.
+            arr[k] = x[j]  # 남은 오른쪽 붙임
             j += 1
+        # 왼쪽 맨 앞 과 오른쪽 맨 앞, 둘 중 더 작은 것을 골라 붙임
         elif x[i] <= x[j]:
             arr[k] = x[i]
             i += 1
