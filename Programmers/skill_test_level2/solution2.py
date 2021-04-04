447293deb2128666adcee9a4aa46ff13d80d6499
HyunJung <HyunJungC.Dev@gmail.com>
2021-04-04 23:27:43 +0900

    fix: Change directory
    change directory

--- Programmers/skill_test_level2/solution2.py
+++ Programmers/skill_test_level2/solution2.py
@@ -1,10 +1,11 @@
 def solution(cacheSize, cities):
     cache = []
     running_time = 0
-    cities = list(map(lambda city: city.lower(), cities))
+    num_cities = len(cities)
+    cities = map(lambda city: city.lower(), cities)
     for ref in cities:
         if cacheSize == 0:
-            return len(cities)*5
+            return num_cities*5
         if not ref in cache:
             if len(cache) < cacheSize:
                 cache.append(ref)
