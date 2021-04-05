ff5b83f3e51bf791e65e1952059daac697702614
HyunJung <HyunJungC.Dev@gmail.com>
2021-04-05 13:11:52 +0900

    fix: Fix more clean code

--- Programmers/level4/징검다리.py
+++ Programmers/level4/징검다리.py
@@ -1,28 +1,28 @@
 def solution(distance, rocks, n):
     answer = 0
-
     rocks.sort()
     rocks.append(distance)
-
-    left, right = 0, distance
+    left = 0
+    right = distance
+    answer = 0
     while left <= right:
-        mid = (left + right) // 2
+        mid = left + (right-left)//2
+        removed_rocks_cnt = 0
+        curr = 0
         min_distance = float('inf')
-        current = 0
-        remove_cnt = 0
-
         for rock in rocks:
-            between_dis = rock - current
-            if between_dis < mid:
-                remove_cnt += 1
-            else:
-                current = rock
-                min_distance = min(min_distance, between_dis)
-
-        if remove_cnt > n:
-            right = mid - 1
-        else:
+            # mid 보다 작으면 다 지운다. = 그래야 mid가 최소값이 된다.
+            between_distance = rock - curr
+            if between_distance >= mid:
+                curr = rock
+                min_distance = min(min_distance, between_distance)
+            elif between_distance < mid:
+                removed_rocks_cnt += 1
+                if removed_rocks_cnt > n:
+                    right = mid - 1
+                    break
+        if removed_rocks_cnt <= n:
+            left = mid+1
             answer = min_distance
-            left = mid + 1
 
     return answer
