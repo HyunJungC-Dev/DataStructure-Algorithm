c47f6f748aa7fcb89a2a0097ad00aa341945811f
HyunJung <HyunJungC.Dev@gmail.com>
2021-04-06 16:55:03 +0900

    feat: Practice to solve Problem - baekjoon 12865
    by memoization

---
+++ Baekjoon/DynamicProgramming/12865번-평범한배낭.py
@@ -0,0 +1,21 @@
+# n : 물품의 수, k : 버틸 수 있는 무게
+n, k = map(lambda x: int(x), input().split())
+
+cargo = [(-1, -1)]
+cnt = n
+while cnt:
+    w, v = map(lambda x: int(x), input().split())
+    cargo.append((w, v))
+    cnt -= 1
+
+dp = [[0 for col in range(k+1)] for row in range(n+1)]
+
+for c_idx in range(1, n+1):
+    for curr_k in range(1, k+1):
+        if cargo[c_idx][0] > curr_k:
+            dp[c_idx][curr_k] = dp[c_idx-1][curr_k]
+        else:
+            dp[c_idx][curr_k] = max(
+                dp[c_idx-1][curr_k],
+                dp[c_idx-1][curr_k-cargo[c_idx][0]] + cargo[c_idx][1])
+print(dp[-1][-1])
