e2a382e5981820f42aeb2de6e764793bf2fb890c
HyunJung <HyunJungC.Dev@gmail.com>
2021-04-06 21:22:28 +0900

    fix: Fix typo
    fix typo

--- Theory/SearchAlgorithm/BinarySearch.py
+++ Theory/SearchAlgorithm/BinarySearch.py
@@ -1,8 +1,7 @@
 def solution(array, target_value):
-    answer = 0
     array.sort()
     left = 0
-    right = len(x)-1
+    right = len(array)-1
     while left <= right:
         mid = (left+right)//2
         if array[mid] == target_value:
