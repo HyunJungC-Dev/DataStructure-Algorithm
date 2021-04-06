4e092df6a708f0ff839805d92e58d5c2c85bbcff
HyunJung <HyunJungC.Dev@gmail.com>
2021-04-06 21:21:50 +0900

    perf: Reduce used memory size
    reduce used memory size

--- Baekjoon/DynamicProgramming/12865번-평범한배낭.py
+++ Baekjoon/DynamicProgramming/12865번-평범한배낭.py
@@ -8,14 +8,14 @@ while cnt:
     cargo.append((w, v))
     cnt -= 1
 
-dp = [[0 for col in range(k+1)] for row in range(n+1)]
+dp = [[0 for col in range(k+1)] for row in range(2)]
 
 for c_idx in range(1, n+1):
     for curr_k in range(1, k+1):
         if cargo[c_idx][0] > curr_k:
-            dp[c_idx][curr_k] = dp[c_idx-1][curr_k]
+            dp[c_idx % 2][curr_k] = dp[(c_idx-1) % 2][curr_k]
         else:
-            dp[c_idx][curr_k] = max(
-                dp[c_idx-1][curr_k],
-                dp[c_idx-1][curr_k-cargo[c_idx][0]] + cargo[c_idx][1])
-print(dp[-1][-1])
+            dp[c_idx % 2][curr_k] = max(
+                dp[(c_idx-1) % 2][curr_k],
+                dp[(c_idx-1) % 2][curr_k-cargo[c_idx][0]] + cargo[c_idx][1])
+print(dp[n % 2][k])
