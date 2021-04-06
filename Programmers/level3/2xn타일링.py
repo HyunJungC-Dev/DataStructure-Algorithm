f81a9a88680f5dd77f61090f861cc53c4293e056
HyunJung <HyunJungC.Dev@gmail.com>
2021-04-06 23:35:46 +0900

    fix: Fix mod value
    fix mod value

--- Programmers/level3/2xn타일링.py
+++ Programmers/level3/2xn타일링.py
@@ -1,7 +1,4 @@
 # 점화식 dp[n] = dp[n-1] + dp[n-2]
-n = int(input())
-
-
 def solution(n):
     if n < 3:
         return n
@@ -9,7 +6,4 @@ def solution(n):
     y = 2
     for _ in range(n-2):
         x, y = y, x+y
-    return y % 10007
-
-
-print(solution(n))
+    return y % 1000000007
