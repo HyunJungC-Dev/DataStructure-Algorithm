544cb4ff38894057ea096de815467063d7df1303
HyunJung <HyunJungC.Dev@gmail.com>
2021-04-16 13:11:47 +0900

    feat: Practice to solve the problem - BOJ2806
    practice to solve the problem - BOJ2806

---
+++ Baekjoon/algoithm-study/week2/2805번-나무자르기.py
@@ -0,0 +1,35 @@
+n, m = map(int, input().split(' '))
+
+tree = list(map(int, input().split(' ')))
+tree.sort(reverse=True)
+
+max_h = max(tree)
+
+answer = 0
+
+
+def binary_search(s, e):
+    global answer
+    if s > e:
+        return
+    mid = s+(e - s)//2
+    wood = 0
+    for t in tree:
+        if t <= mid:
+            break
+        else:
+            wood += t-mid
+    if wood > m:
+        answer = mid
+        binary_search(mid+1, e)
+
+    elif wood < m:
+        binary_search(s, mid-1)
+
+    elif wood == m:
+        answer = mid
+        return
+
+
+binary_search(0, max_h)
+print(answer)
