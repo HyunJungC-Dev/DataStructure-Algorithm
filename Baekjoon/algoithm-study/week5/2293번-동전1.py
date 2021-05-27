e80946150473e0218ee16d08ac9c2e78d3c1e9bb
HyunJung <HyunJungC.Dev@gmail.com>
2021-05-28 01:59:14 +0900

    fix: Remove remark
    주석 제거

--- Baekjoon/algoithm-study/week5/2293번-동전1.py
+++ Baekjoon/algoithm-study/week5/2293번-동전1.py
@@ -2,7 +2,7 @@ n, k = map(int, input().split(' '))
 coins = [int(input()) for _ in range(n)]
 dp = [0 for _ in range(k+1)]  # dp[i] = i원을 만들 수 있는 코인의 수
 
-dp[0] = 1
+dp[0] = 1  # 이게 n원으로 n을 만들 때 여길 참조하기 때문
 for i in coins:
     for j in range(i, k+1):
         if j-i >= 0:
