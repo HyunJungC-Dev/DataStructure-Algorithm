18fc8b55a7c4f61c5573e37a2414941db48592d9
HyunJung <HyunJungC.Dev@gmail.com>
2021-05-31 15:55:14 +0900

    fix: Fix wrong algorithm
    not j, It is i

--- Theory/SortAlgorithm/BubbleSort.py
+++ Theory/SortAlgorithm/BubbleSort.py
@@ -3,7 +3,7 @@ def bubble_sort(x):
     length = len(x)-1  # 자기 다음과 비교하므로 마지막 요소는 확인할 필요 없음
     for i in range(length):  # 이 for 문을 한번 돌면 가장 큰 게 맨 뒤로 가 있다.
         swapped = False
-        for j in range(length-j):  # 자기 앞은 보지 않음.
+        for j in range(length-i):  # 자기 앞은 보지 않음.
             if x[j] > x[j+1]:  # 앞이 뒤보다 크면
                 swapped = True
                 x[j], x[j+1] = x[j+1], x[j]  # swap
